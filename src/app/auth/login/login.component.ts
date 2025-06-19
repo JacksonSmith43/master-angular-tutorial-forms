d52b6741ab761e0123b5f6788174c1fe007ccd5e
JacksonSmith43 <jessica444@gmx.at>
2025-06-19 09:18:08 +0200

    252. Building Custom Validators.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes("?")) {
+    return null; // Null means that the validation passed. 
+  }
+  return { doesNotContainQuestionMark: true } // Return an object with a key to indicate the validation failed. 
+}
 
 @Component({
   selector: 'app-login',
@@ -15,7 +22,7 @@ export class LoginComponent {
       validators: [Validators.email, Validators.required]
     }),
     password: new FormControl("", {
-      validators: [Validators.minLength(6), Validators.required]
+      validators: [Validators.minLength(6), Validators.required, mustContainQuestionMark]
     })
   });
 
