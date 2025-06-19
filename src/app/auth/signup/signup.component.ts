696e9efe59b03b48193b3130f90fa0d778bd00de
JacksonSmith43 <jessica444@gmx.at>
2025-06-19 11:54:48 +0200

    257. Connecting & Registering Inputs For A Complex Form.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -16,14 +16,20 @@ export class SignupComponent {
     }),
     password: new FormControl("", {
       validators: [Validators.minLength(6), Validators.required]
-    })
+    }),
+    confirmPassword: new FormControl("", { validators: [Validators.minLength(6), Validators.required] }),
+    firstName: new FormControl("", { validators: [Validators.required] }),
+    lastName: new FormControl("", { validators: [Validators.required] }),
+    street: new FormControl("", { validators: [Validators.required] }),
+    number: new FormControl("", { validators: [Validators.required] }),
+    postCode: new FormControl("", { validators: [Validators.required] }),
+    city: new FormControl("", { validators: [Validators.required] }),
+    role: new FormControl<"student" | "teacher" | "employee" | "founder" | "other">("student", { validators: [Validators.required] }),
+    agree: new FormControl(false, { validators: [Validators.required] })
   })
 
   onSubmit() {
-    const enteredEmail = this.form.value.email;
-    const enteredPassword = this.form.value.password;
-
-    console.log("enteredEmail: ", enteredEmail);
+    console.log(this.form);
   }
 
   onReset() {
